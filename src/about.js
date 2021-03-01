29383b545ee65c7b48d4bc94871648dbd880da98
Philippe Gonse <pgonse@gmail.com>
2021-03-01 14:02:21 -0500

    add portfolio to project and modify about text

--- src/about.js
+++ src/about.js
@@ -8,20 +8,20 @@ function About() {
       <h1>About-me</h1>
       <div className="about-info">
         <p>
-          Originaly from the aviation world, 2020 lead me to develop my skills as a
-          web developer.
+          I am Philippe, Web Developer located in Montreal. I enjoy to design good UI,
+          animation and creative layouts.
           <br />
           <br />
-          Always passionate about tech world, I went on this journey and learn a
-          lot at Le Wagon bootcamp. Since, I&apos;m keeping the momentum and
+          Always passionate about tech world, I discover coding in 2020
+          and learned a lot at Le Wagon bootcamp. Since, I&apos;m keeping the momentum and
           keep learning on a daily basis.
           <br />
-          Well organised person, problem solver, and independent employee
+          I am a well organised person, problem solver, and independent employee
           with good attention to details.
           <br />
           <br />
-          Experience in full-stack, I really like to design good
-          front-end and will be pleased to hear about all kind of projects.
+          More oriented in Front-end, I have experience in full-stack development aswell.
+          Don&apos;t hesitate to reach me, I will be pleased to hear about your project.
         </p>
         <img src={me} alt="" />
       </div>
--- src/app.js
+++ src/app.js
@@ -8,9 +8,10 @@ import Footer from "./footer";
 
 import troismats from "./assets/img/troismats.PNG";
 import helper from "./assets/img/helper.JPG";
+import capture from "./assets/img/capture.JPG";
 
 import css from "./assets/img/icon/css.png";
-// import firebase from "./assets/img/icon/firebase.png";
+import firebase from "./assets/img/icon/firebase.png";
 import github from "./assets/img/icon/github.png";
 import html from "./assets/img/icon/html.png";
 import js from "./assets/img/icon/js.png";
@@ -31,12 +32,12 @@ function App() {
         <About />
       </div>
       <section id="projects" className="project-section">
-        <h1>Some of my Work</h1>
+        <h1>Past Projects</h1>
         <Projects
           troismats={troismats}
           title='Les Trois Mats'
           subtitle='The appartment for your holidays!'
-          description={<>This website was design and made in one week.<br /> I used the React framework to build all the pages, google maps api to display the maps and emailjs for the contact form.<br />I used Netlify for the deployement.</>}
+          description={<>This website was design and made in one week.<br /> I used the React framework to build all the pages, google maps api to display the maps and emailjs for the contact form.<br />It is deployed using Netlify.</>}
           link="https://les-trois-mats.com"
           id="1"
           receivedIcon={[{name: "Html", img: html}, {name: "Css", img: css}, {name: "Javascript", img: js}, {name: 'React', img: react}, {name: "Github", img: github}, {name: "Netlify", img: netlify}]}
@@ -49,6 +50,15 @@ function App() {
           link="https://helper.solutions"
           id="2"
           receivedIcon={[{name: 'Html', img: html}, {name: 'Css', img: css}, {name: 'Javascript', img: js}, {name: 'Ruby on Rails', img: rails}, {name: 'Github', img: github}, {name: 'Heroku', img: ""}]}
+        />
+        <Projects
+          troismats={capture}
+          title="My Portfolio"
+          subtitle=""
+          description={<>I used React to build this Portfolio.<br />Each project is displayed with a reusable component.<br />It is deployed with Firebase.</>}
+          link="google.ca"
+          id="3"
+          receivedIcon={[{name: "Html", img: html}, {name: "Css", img: css}, {name: "Javascript", img: js}, {name: 'React', img: react}, {name: "Github", img: github}, {name: "Firebase", img: firebase}]}
         />
       </section>
       <Footer />
