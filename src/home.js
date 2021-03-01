152c7f83d9fca6186530949004edad2f16501579
Philippe Gonse <pgonse@gmail.com>
2021-03-01 11:38:09 -0500

    make a mobile navbar

--- src/home.js
+++ src/home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="hi">
+    <div id="home" className="hi">
       <h1>
         Hello,
         <br />
--- src/navbar.js
+++ src/navbar.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
+  const [display, setDisplay] = useState("none");
+
+  const handleClick = () => {
+    display === "none" ? setDisplay("flex") : setDisplay("none")
+  };
+
   return (
+    <>
     <div className="navbar">
       <h3 className="my-portfolio">My Portfolio<span style={{ color: "red"}}>.</span></h3>
       <div className="navbar-menu">
@@ -21,9 +28,24 @@ function Navbar() {
         </a>
       </div>
       <div className="navbar-menu-mobile">
-        <FontAwesomeIcon icon={faBars} />
+        <FontAwesomeIcon icon={faBars} onClick={() => handleClick()} />
       </div>
     </div>
+    <div style={{ display }} className="menu-mobile">
+      <a className="navbar-link" href="#home" onClick={() => handleClick()}>
+        <h3 style={{ fontWeight: "lighter" }}>Home</h3>
+      </a>
+      <a className="navbar-link" href="#about" onClick={() => handleClick()}>
+        <h3 style={{ fontWeight: "lighter" }}>About</h3>
+      </a>
+      <a className="navbar-link" href="#projects" onClick={() => handleClick()}>
+        <h3 style={{ fontWeight: "lighter" }}>Projects</h3>
+      </a>
+      <a className="navbar-link" href="#contact" onClick={() => handleClick()}>
+        <h3 style={{ fontWeight: "lighter" }}>Contact</h3>
+      </a>
+    </div>
+    </>
   );
 }
 
