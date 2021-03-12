e49e6d57d2be522d5220a6d6d01fadf0ebaef21e
Philippe Gonse <pgonse@gmail.com>
2021-03-12 15:56:54 -0500

    eye animation in % and start in middle

--- src/svg.js
+++ src/svg.js
@@ -8,7 +8,7 @@ function Svg() {
           <line x1="45" x2="75" y1="55" y2="85" stroke="black" strokeWidth="5"/>
           <line x1="125" x2="125" y1="35" y2="75" stroke="black" strokeWidth="5"/>
           <line x1="205" x2="175" y1="55" y2="85" stroke="black" strokeWidth="5"/>
-          <ellipse cx="125" cy="125" rx="75" ry="35" stroke="black" fill="transparent" strokeWidth="4"/>
+          <ellipse cx="125" cy="125" rx="75" ry="35" stroke="black" fill="transparent" strokeWidth="4" />
           <circle cx="125" cy="125" r="20" className="svg-circle" />
         </svg>
       </div>
